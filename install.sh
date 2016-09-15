7806a78a130b49ff65040091e60a6ee95957fba7
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-09-15 13:07:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,59 +4,30 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew install cask
 
-brew cask install \
-  appcleaner \
-  coconutbattery \
-  dash \
-  deezer \
-  docker \
-  dropbox \
-  elm-platform \
-  firefox \ 
-  google-chrome \
-  hex-fiend \
-  iterm2 \
-  java \ 
-  keka \
-  monosnap \
-  ngrok \
-  libreoffice \
-  skype \
-  slack \
-  sourcetree \
-  sqlpro-for-postgres \
-  transmission \
-  torbrowser
-  vagrant \
-  virtualbox \
-  vlc
+brew cask install appcleaner \
+                  coconutbattarey \
+                  dash deezer docker dropbox \
+                  elm-platform \
+                  firefox \ 
+                  google-chrome \
+                  hex-fiend \
+                  iterm2 \
+                  java \ 
+                  keka \
+                  libreoffice \
+                  monosnap \
+                  ngrok \
+                  skype slack sourcetree sqlpro-for-postgres \
+                  transmission torbrowser vagrant virtualbox vlc
 
-brew install \
-
-  # Git
-  git \
-  git-flow \
-  git-lfs \
-  git-core \
-
-  # Vim
-  vim \
-  jq \
-  ack \
-  ctags \
-
-  # Languages
-  elixir \
-  erlang \
-  lua \
-
-  # Other
-  ansible \
-  elasticsearch \
-  imagemagick \
-  openssl \
-  nodejs \
-  postgresql \
-  redis \
-  ssh-copy-id
-
+brew install git git-flow git-lfs \
+            vim jq ack ctags \
+            elixir erlang lua \
+            ansible \
+            elasticsearch \
+            imagemagick \
+            openssl \
+            nodejs \
+            postgresql \
+            redis \
+            ssh-copy-id
