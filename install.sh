06fe914ffc7c25e94f5779bee7efcda92079ebb1
Marat Khusnetdinov <khusnetdinov@users.noreply.github.com>
2016-09-16 12:56:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install cask
 
 brew cask install appcleaner \
-                  coconutbattarey \
-                  dash deezer docker dropbox \
-                  elm-platform \
-                  firefox \ 
-                  google-chrome \
-                  hex-fiend \
-                  iterm2 \
-                  java \ 
-                  keka \
-                  libreoffice \
-                  monosnap \
-                  ngrok \
-                  skype slack sourcetree sqlpro-for-postgres \
-                  transmission torbrowser vagrant virtualbox vlc
+            coconutbattarey \
+            dash deezer docker dropbox \
+            elm-platform \
+            firefox \ 
+            google-chrome \
+            hex-fiend \
+            iterm2 \
+            java \ 
+            keka \
+            libreoffice \
+            monosnap \
+            ngrok \
+            skype slack sourcetree sqlpro-for-postgres \
+            transmission torbrowser vagrant virtualbox vlc
 
 brew install git git-flow git-lfs \
             vim jq ack ctags \
